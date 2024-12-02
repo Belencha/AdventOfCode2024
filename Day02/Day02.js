2292b18976b522246506b8b98360c7a3948d1d3f
Belén Moreno Hurtado <bmoreno@gmv.com>
2024-12-02 23:53:33 +0100

    Calculate the number of safe reports for Day 2 Part One

--- Day02/Day02.js
+++ Day02/Day02.js
@@ -1,3 +1,5 @@
+const fs = require("fs").promises;
+
 const parseData = (input) => {
   const matrix = [];
 
@@ -39,6 +41,46 @@ const countSafeReports = (matrix) => {
     0
   );
 };
+
+const readInputDataFile = async () => {
+  const matrix = [];
+
+  try {
+    const data = await fs.readFile("Day02.input.txt", "utf8");
+
+    // Split the content by lines
+    const lines = data.trim().split("\n");
+
+    // Loop through each line and split by space to get the values
+    lines.forEach((line) => {
+      const values = line.trim().split(/\s+/).map(Number);
+      matrix.push(values);
+    });
+
+    return matrix;
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return matrix; // return empty lists in case of an error
+  }
+};
+
+const main = async () => {
+  const inputData = `3   4\n4   3\n2   5\n1   3\n3   9\n3   3`;
+
+  //const result = parseData(inputData);
+  const result = await readInputDataFile();
+
+  console.log({ result });
+
+  const numberOfSafeReports = countSafeReports(result);
+
+  console.log({ numberOfSafeReports });
+};
+
+// Execute main function
+if (require.main === module) {
+  main();
+}
 
 module.exports = {
   parseData,
