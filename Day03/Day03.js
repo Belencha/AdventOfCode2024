45f684a1d89c9b5da081ff69fd4d65f56726f28d
Belén Moreno Hurtado <bmoreno@gmv.com>
2024-12-04 23:27:17 +0100

    Calculate the sum of all mul for a hardcoded example

--- Day03/Day03.js
+++ Day03/Day03.js
@@ -20,6 +20,22 @@ const calculateTotal = (multipliersArray) => {
   return multipliedValues.reduce((acc, current) => acc + current, 0);
 };
 
+const main = async () => {
+  const inputData = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+
+  const result = parseData(inputData);
+  //const result = await readInputDataFile();
+
+  const totalMulCalculation = calculateTotal(result);
+
+  console.log({ totalMulCalculation });
+};
+
+// Execute main function
+if (require.main === module) {
+  main();
+}
+
 module.exports = {
   parseData,
   calculateTotal,
